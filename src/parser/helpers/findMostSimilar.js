e9fa2abf0205aee0b111e3cad14b660893e43fa7
Michail Zyusko <michail.zyusko@gmail.com>
2022-02-12 20:48:33 +0300

    v1.0.12

--- src/parser/helpers/findMostSimilar.js
+++ src/parser/helpers/findMostSimilar.js
@@ -2,17 +2,17 @@ import cherio from 'cherio';
 import compare from 'string-similarity';
 
 export default async (substane, listHtml) => {
-    let $ = await cherio.load(listHtml);
-    const arr = Array.from($('.mzr-tree-node'));
-    const names = arr.map(({ children: [el] }) => el?.data);
-    const { ratings } = compare.findBestMatch(substane, names);
-    let bestMatch = { rating: 0 };
-    ratings.forEach(({ rating, target }, index) => {
-        if (rating > bestMatch.rating) {
-            bestMatch = { rating, target, index };
-        }
-    });
-    console.log('Best match: ',bestMatch);
+  const $ = await cherio.load(listHtml);
+  const arr = Array.from($('.mzr-tree-node'));
+  const names = arr.map(({ children: [el] }) => el?.data);
+  const { ratings } = compare.findBestMatch(substane, names);
+  let bestMatch = { rating: 0 };
+  ratings.forEach(({ rating, target }, index) => {
+    if (rating > bestMatch.rating) {
+      bestMatch = { rating, target, index };
+    }
+  });
+  console.log('Best match: ', bestMatch);
 
-    return bestMatch;
-}
+  return bestMatch;
+};
